ac3a4db957a56a02aaca5e1c4f300de40ec1aae9
Henry Blackie <he.blackie@gmail.com>
2025-07-22 11:25:23 +0100

    Updated logic

--- spider.py
+++ spider.py
@@ -13,15 +13,18 @@ class NodeItem(scrapy.Item):
     id = scrapy.Field()
     label = scrapy.Field()
 
+
 class FirstEdgeItem(scrapy.Item):
     """Item to represent the first edge in the graph."""
     source = scrapy.Field()
     target = scrapy.Field()
 
+
 class EdgeItem(scrapy.Item):
     """Item to represent an edge in the graph."""
     source = scrapy.Field()
     target = scrapy.Field()
+
 
 class IncelswikiSpider(scrapy.Spider):
     """Spider to scrape the Incels Wiki and archive pages locally."""
@@ -30,7 +33,8 @@ class IncelswikiSpider(scrapy.Spider):
         super().__init__()
         # Capture the timestamp once during initialization
         self.timestamp = datetime.now().strftime('%Y%m%d-%H%M')
-        self.outlink_pattern = re.compile(r'\/w\/(?!File)(?!Category)(?!Editing_rules)[^#\t\n\r]+')
+        self.outlink_pattern = re.compile(
+            r'\/w\/(?!File)(?!Category)(?!Editing_rules)[^#\t\n\r]+')
 
     name = 'incelswiki'
     allowed_domains = ['incels.wiki']
@@ -55,14 +59,14 @@ class IncelswikiSpider(scrapy.Spider):
             }
         },
         'AUTOTHROTTLE_ENABLED': True,
-        'DEPTH_LIMIT': 2,
+        'DEPTH_LIMIT': 10,
         'REDIRECT_ENABLED': True,
         'LOGSTATS_INTERVAL': 300,
         'LOG_LEVEL': 'INFO'
     }
 
     def parse(self, response):
-        self.logger.info(f'Parsing {response.url}')
+        self.logger.debug(f'Parsing {response.url}')
         """Parse the response from the Incels Wiki and extract nodes and edges."""
         # Update archives
         self.save_to_local_archive(response)
@@ -70,7 +74,13 @@ class IncelswikiSpider(scrapy.Spider):
 
         # Extract the title and outlinks from the response
         title = response.css('#firstHeading span::text').get()
-        outlinks = response.css('#mw-content-text a::attr(href)').getall()
+        # outlinks = response.css('#mw-content-text a::attr(href)').getall()
+        outlinks = response.css(
+                '#mw-content-text p a::attr(href), '
+                '#mw-content-text ul a::attr(href), '
+                '#mw-content-text ol a::attr(href), '
+                '.redirectText a::attr(href)'
+            ).getall()
 
         # Yield the node details
         # yield {'id': response.url, 'label': title if title else 'No title'}
@@ -89,28 +99,42 @@ class IncelswikiSpider(scrapy.Spider):
 
         # Crawl valid internal links in the body
         first_outlink_yielded = False
+        first_outlink = True
         for href in outlinks:
             # Only follow internal wiki article links (not files, categories, etc.)
             if self.outlink_pattern.match(href):
                 target_url = urljoin(response.url, href)
+                # target_url = requests.get(
+                #     target_url, allow_redirects=True).url  # Resolve redirects
 
-                yield EdgeItem(source=response.url, target=target_url)
+                # yield EdgeItem(source=response.url, target=target_url)
 
-                if not first_outlink_yielded:
-                    # Yield the first outlink as an edge
-                    # yield {
-                    #     'source': response.url,
-                    #     'target': target_url
-                    # }
-                    yield FirstEdgeItem(source=response.url, target=target_url)
-                    first_outlink_yielded = True
-                    
-                    # Only follow the first outlink
-                    self.logger.info(f'{title} ---> {href}')
-                    yield scrapy.Request(target_url, callback=self.parse)
-        
-        if not first_outlink_yielded:
-            self.logger.warning(f'{title} ---> None')
+                yield scrapy.Request(target_url,
+                                     callback=self.edge_callback,
+                                     meta={
+                                         'source': response.url,
+                                         'first': first_outlink
+                                     })
+                first_outlink = False
+
+                # if not first_outlink_yielded:
+                #     # Yield the first outlink as an edge
+                #     # yield {
+                #     #     'source': response.url,
+                #     #     'target': target_url
+                #     # }
+                #     yield FirstEdgeItem(source=response.url, target=target_url)
+                #     first_outlink_yielded = True
+
+                #     # Only follow the first outlink
+                #     self.logger.info(f'{title} ---> {href}')
+                #     yield scrapy.Request(target_url, callback=self.parse)
+
+                # yield scrapy.Request(target_url, callback=self.parse)
+
+        # If no valid outlinks were found, log a warning
+        if first_outlink:
+            self.logger.warning(f'{response.url} ---> None')
             # yield {
             #             'source': response.url,
             #             'target': None
@@ -166,6 +190,19 @@ class IncelswikiSpider(scrapy.Spider):
         #         if line.strip() != ',':
         #             outfile.write(line)
 
+    def edge_callback(self, response):
+        source = response.meta['source']
+        first = response.meta['first']
+
+        # Use response.url as canonical target
+        yield EdgeItem(source=source, target=response.url)
+        if first:
+            yield FirstEdgeItem(source=source, target=response.url)
+            self.logger.info(f'{source} ---> {response.url}')
+
+        # Continue crawling
+        yield from self.parse(response)
+
     def save_to_local_archive(self, response):
         """Save the response body to a local archive."""
         # Generate a filename based on the page title
@@ -185,19 +222,24 @@ class IncelswikiSpider(scrapy.Spider):
                 self.logger.debug(f'Saved {f.name}')
 
             # Save the main body images
-            for img_url in response.css('#mw-content-text img::attr(src)').getall():
+            for img_url in response.css(
+                    '#mw-content-text img::attr(src)').getall():
                 img_url = urljoin(response.url, img_url)
                 img_name = img_url.split('/')[-1]
                 # img_path = os.path.join(directory, filename_timestamp, img_name)
                 try:
-                    img_response = requests.get(img_url, timeout=10)
+                    img_response = requests.get(
+                        img_url,
+                        timeout=10,
+                        headers={'User-Agent': 'Mozilla/5.0'})
                     img_response.raise_for_status()
-                    with open(os.path.join(directory, img_name), 'wb') as img_file:
+                    with open(os.path.join(directory, img_name),
+                              'wb') as img_file:
                         img_file.write(img_response.content)
                     self.logger.debug(f'Saved {img_name}.')
                 except Exception as e:
                     self.logger.warning(f'Failed to save image {img_url}: {e}')
-    
+
     def save_to_wayback(self, url):
         """Save the response to the Wayback Machine."""
         # Wayback Machine API endpoint
@@ -207,10 +249,10 @@ class IncelswikiSpider(scrapy.Spider):
         self.logger.debug(f'Saving {url} to Wayback Machine...')
         try:
             response = requests.get(wayback_api + url)
-            
+
             # Raise an error for bad responses
-            response.raise_for_status()  
-            
+            response.raise_for_status()
+
             self.logger.debug(f"Submitted {url} to Wayback Machine.")
         except Exception as e:
             self.logger.warning(f"Unable to archive page to Wayback Machine.")
